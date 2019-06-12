0649895e9113fdae9e7bf570a569c4da54421af4
kszheng <sunsetzks@gmail.com>
2019-06-12 20:12:55 +0800

    no

---
+++ test/get_res.py
@@ -0,0 +1,35 @@
+from PIL import Image
+from pylab import *
+
+import os
+
+ifs = ['input_%d.bmp' % (i) for i in range(0, 100)]
+ofs = ['output_%d.bmp' % (i) for i in range(0, 100)]
+
+d = 'save/video/'
+ips = [Image.open(d+i) for i in ifs]
+ops = [Image.open(d+i) for i in ofs]
+
+
+# figure(figsize=(20,20), dpi=96)
+# plt.rcParams['figure.figsize'] = [10, 5]
+
+fig, axes =plt.subplots(10,10, figsize=(20, 20), facecolor='w', edgecolor='k')
+
+for i, ipt in enumerate(ips):
+    ax = axes[i//10, i%10]
+    ax.imshow(ipt)
+    ax.axis('off')
+    ax.set_title('input%d'%(i))
+
+plt.savefig('inputs1.png')
+
+fig, axes =plt.subplots(10,10, figsize=(20, 20), facecolor='w', edgecolor='k')
+
+for i, ipt in enumerate(ops):
+    ax = axes[i//10, i%10]
+    ax.imshow(ipt)
+    ax.axis('off')
+    ax.set_title('outputs%d'%(i))
+
+plt.savefig('outputs1.png')
