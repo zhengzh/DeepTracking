05411a02d3c6a002bbe056d4efa5fca09a7114a8
zks <zks>
2019-06-28 08:16:25 +0800

    no

--- test/model_conv.py
+++ test/model_conv.py
@@ -52,9 +52,13 @@ class Net(nn.Module):
         d3 = 9
         d4 = 27
         self.l1 = nn.Conv2d(4, h, (3, 3), (1, 1), (1, 1), dilation=(1,1))
+        self.l11 = nn.Conv2d(h, h, (3, 3), (1, 1), (1, 1), dilation=(1,1))
         self.l2 = nn.Conv2d(h, h, (3, 3), (1, 1), (d2, d2), dilation=(d2,d2))
+        self.l21 = nn.Conv2d(h, h, (3, 3), (1, 1), (d2, d2), dilation=(d2,d2))
         self.l3 = nn.Conv2d(h, h, (3, 3), (1, 1), (d3, d3), dilation=(d3,d3))
+        self.l31 = nn.Conv2d(h, h, (3, 3), (1, 1), (d3, d3), dilation=(d3,d3))
         self.l4 = nn.Conv2d(h, h, (3, 3), (1, 1), (d4, d4), dilation=(d4,d4))
+        self.l41 = nn.Conv2d(h, h, (3, 3), (1, 1), (d4, d4), dilation=(d4,d4))
         
         # input_channel, output_channel, kernel, stride, padding, dialation
         self.output = nn.Conv2d(4*h, 64, (3, 3), (1, 1), (1, 1), dilation=(1,1))
@@ -65,9 +69,13 @@ class Net(nn.Module):
     def forward(self, x):
         m = torch.relu
         h1 = m(self.l1(x))
+        h1 = m(self.l11(h1))
         h2 = m(self.l2(h1))
+        h2 = m(self.l21(h2))
         h3 = m(self.l3(h2))
+        h3 = m(self.l31(h3))
         h4 = m(self.l4(h3))
+        h4 = m(self.l41(h4))
 
         cat = torch.cat((h1, h2, h3, h4), 1)
         # cat = h1 + h3
